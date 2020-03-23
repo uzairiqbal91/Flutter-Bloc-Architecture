9878a9e5265c9d05fd88ae6214f91c57d8a7ea5d
uzair <uzair@fomarkmedia.com>
2020-03-23 23:36:20 +0500

    provider package integrated

---
+++ flutter_flare/lib/HomeScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          color: Colors.white,
+          child: Center(
+            child: Text("Home Screen"),
+          ),
+        ),
+      ),
+    );
+  }
+}
